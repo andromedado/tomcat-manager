3463aec7f22e3c2842d05dc5d23b1d8804f176cb
Shad Downey <shad.downey@gmail.com>
2017-04-01 09:37:43 -0400

    Initial Commit

---
+++ TomcatManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TomcatManager
+//
+//  Created by Shad Downey on 4/1/17.
+//  Copyright © 2017 Shad Downey. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TomcatManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TomcatManager
+//
+//  Created by Shad Downey on 4/1/17.
+//  Copyright © 2017 Shad Downey. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
